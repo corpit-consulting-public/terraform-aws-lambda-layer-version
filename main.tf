9dca9d8e66a3b815b7c6a0bcb6286b56f582dcb1
Circular-test <nmartinez@corpit-consulting.com>
2019-07-22 17:39:38 -0300

    fix lambda layer

---
+++ main.tf
@@ -0,0 +1,8 @@
+resource "aws_lambda_layer_version" "lambda_layer" {
+  #filename           = "lambda_layer_payload.zip"
+  layer_name          = "${var.layer_name}"
+  description         = "${var.description}"
+  compatible_runtimes = ["${var.compatible_runtimes}"]
+  s3_bucket           = "${var.s3_bucket}"
+  s3_key              = "${var.s3_key}"
+}
