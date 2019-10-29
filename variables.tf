e477324658e0310d918f3f8f1a7170eca82ace4c
Nicolas Martinez <nmartinez@corpit-consulting.com>
2019-10-29 10:11:41 -0300

    support for tf12

--- variables.tf
+++ variables.tf
@@ -1,51 +1,52 @@
 variable "layer_name" {
-  type        = "string"
+  type        = string
   description = "A unique name for your Lambda Layer"
 }
 
 variable "filename" {
-  type        = "string"
+  type        = string
   description = "The path to the function's deployment package within the local filesystem. If defined, The s3_-prefixed options cannot be used."
   default     = ""
 }
 
 variable "s3_bucket" {
-  type        = "string"
+  type        = string
   description = "The S3 bucket location containing the function's deployment package. Conflicts with filename. This bucket must reside in the same AWS region where you are creating the Lambda function."
   default     = ""
 }
 
 variable "s3_key" {
-  type        = "string"
+  type        = string
   description = "The S3 key of an object containing the function's deployment package. Conflicts with filename."
   default     = ""
 }
 
 variable "s3_object_version" {
-  type        = "string"
+  type        = string
   description = "he object version containing the function's deployment package. Conflicts with filename."
   default     = ""
 }
 
 variable "description" {
-  type        = "string"
+  type        = string
   description = "Description of what your Lambda Layer does."
 }
 
 variable "compatible_runtimes" {
-  type        = "list"
+  type        = list(string)
   description = "A list of Runtimes this layer is compatible with. Up to 5 runtimes can be specified."
   default     = []
 }
 
 variable "license_info" {
-  type        = "string"
+  type        = string
   description = " License info for your Lambda Layer"
   default     = ""
 }
 
 variable "source_code_hash" {
-  type        = "string"
-  description = "Used to trigger updates. Must be set to a base64-encoded SHA256 hash of the package file specified with either filename or s3_key. The usual way to set this is ${filebase64sha256(file.zip)} (Terraform 0.11.12 or later) or ${base64sha256(file(file.zip))} (Terraform 0.11.11 and earlier), where file.zip is the local filename of the lambda layer source archive."
+  type        = string
+  description = "Used to trigger updates. Must be set to a base64-encoded SHA256 hash of the package file specified with either filename or s3_key. The usual way to set this is $${filebase64sha256(file.zip)} (Terraform 0.11.12 or later) or $${base64sha256(file(file.zip))} (Terraform 0.11.11 and earlier), where file.zip is the local filename of the lambda layer source archive."
   default     = ""
 }
+
