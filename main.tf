46ea452305992b1c768d83cf2948cd7f69975cc0
Nicolas Martinez <nmartinez@corpit-consulting.com>
2020-07-19 15:55:56 -0300

    upgrade module tf 12

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 resource "aws_lambda_layer_version" "lambda_layer" {
   #filename           = "lambda_layer_payload.zip"
-  layer_name          = "${var.layer_name}"
-  description         = "${var.description}"
-  compatible_runtimes = ["${var.compatible_runtimes}"]
-  s3_bucket           = "${var.s3_bucket}"
-  s3_key              = "${var.s3_key}"
-  s3_object_version   = "${var.s3_object_version}"
+  layer_name          = var.layer_name
+  description         = var.description
+  compatible_runtimes = var.compatible_runtimes
+  s3_bucket           = var.s3_bucket
+  s3_key              = var.s3_key
+  s3_object_version   = var.s3_object_version
 }
+
--- output.tf
+++ output.tf
@@ -1,15 +1,15 @@
-
 output "arn" {
   description = "The Amazon Resource Name (ARN) of the Lambda Layer with version."
-  value       = "${aws_lambda_layer_version.lambda_layer.arn}"
+  value       = aws_lambda_layer_version.lambda_layer.arn
 }
 
 output "layer_arn" {
   description = "The Amazon Resource Name (ARN) of the Lambda Layer without version"
-  value       = "${aws_lambda_layer_version.lambda_layer.layer_arn}"
+  value       = aws_lambda_layer_version.lambda_layer.layer_arn
 }
 
 output "version" {
   description = "This Lamba Layer version."
-  value       = "${aws_lambda_layer_version.lambda_layer.version}"
+  value       = aws_lambda_layer_version.lambda_layer.version
 }
+
